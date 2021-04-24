9c0f039234da37bec0d692d03d701e79db25b4b3
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2021-04-24 22:47:30 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -29,28 +29,28 @@ const cart = () => {
           <h6 className="my-0">Product name</h6>
           <small className="text-muted">Brief description</small>
         </div>
-        <span className="text-muted">₹1200</span>
+        <span className="text-muted">$1200</span>
       </li>
       <li className="list-group-item d-flex justify-content-between lh-condensed">
         <div>
           <h6 className="my-0">Second product</h6>
           <small className="text-muted">Brief description</small>
         </div>
-        <span className="text-muted">₹800</span>
+        <span className="text-muted">$800</span>
       </li>
       <li className="list-group-item d-flex justify-content-between lh-condensed">
         <div>
           <h6 className="my-0">Third item</h6>
           <small className="text-muted">Brief description</small>
         </div>
-        <span className="text-muted">₹500</span>
+        <span className="text-muted">$500</span>
       </li>
       <li className="list-group-item d-flex justify-content-between bg-light">
         <div className="text-success">
           <h6 className="my-0">Promo code</h6>
           <small>EXAMPLECODE</small>
         </div>
-        <span className="text-success">-₹500</span>
+        <span className="text-success">-$500</span>
       </li>
       <li className="list-group-item d-flex justify-content-between">
         <span>Total (INR)</span>
